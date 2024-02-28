d2b3903c9d894c73920f7269f5602cb2e8c1781c
virlancristian <rethvol111@gmail.com>
2024-02-28 17:32:58 +0200

    Transfer commit #9

--- backend/src/main/java/com/example/backend/api/common/AcceptedImageFormats.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.api.common;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class AcceptedImageFormats {
-    private static final String[] imageFormatArray = {"jpeg", "jpg", "png"};
-    public static final List<String> IMAGE_FORMATS = new ArrayList<>(List.of(imageFormatArray));
-}
