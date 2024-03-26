a128c1ea7b219b8e90f9da40b2d7cb0d29d0d52f
virlancristian <rethvol111@gmail.com>
2024-03-26 13:52:26 +0200

    Removed redundant console.log in key-press.ts

--- src/frontend/src/events/key-press.ts
+++ src/frontend/src/events/key-press.ts
@@ -1,7 +1,6 @@
 import { KeyboardEvent } from "react";
 
 export function checkForKey(mappedKey: string, event: KeyboardEvent<HTMLElement>, action: any, args: any) {
-    console.log(event.key);
     if(event.key === mappedKey) {
         action(...Object.values(args));
     }
