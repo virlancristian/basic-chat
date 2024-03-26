ef26c59202eb442943edbc10ed8d78f9bbc4d666
virlancristian <rethvol111@gmail.com>
2024-03-26 13:51:08 +0200

    Removed redundant message-options.tsx

--- src/frontend/src/components/home/message-options.tsx
+++
@@ -1,8 +0,0 @@
-export default function MessageOptions() {
-    return <div className="message-options">
-        <ul>
-            <li>Edit</li>
-            <li>Delete</li>
-        </ul>
-    </div>
-}
